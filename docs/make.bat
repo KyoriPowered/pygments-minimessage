a5821a567b0ff2955609ec3582219a97bc84208c
zml <zml@stellardrift.ca>
2025-02-12 23:56:23 -0800

    chore(docs): Add links back to the repository from the docs site

--- docs/make.bat
+++ docs/make.bat
@@ -24,12 +24,17 @@ if errorlevel 9009 (
 )
 
 if "%1" == "" goto help
+if "%1" == "livehtml" goto livehtml
 
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:livehtml
+sphinx-autobuild --re-ignore _build -b dirhtml %SOURCEDIR% %BUILDDIR%/dirhtml %SPHINXOPTS%
+goto end
 
 :end
 popd
